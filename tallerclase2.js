091cd9f43ec6e74cbf108eff6c2b5de9374b76ef
Tu Nombre <tuemail@example.com>
2025-11-24 20:34:55 -0500

    mi primer commit

---
+++ tallerclase2.js
@@ -0,0 +1,22 @@
+// 1. Promedio
+let promedio = (70 + 82 + 50 + 20) / 4;
+console.log(promedio);
+let num1= 6
+// 2. Elevar num1 a la 3
+let num1 = 6;
+let elevado = num1 ** 3;
+console.log(elevado);
+
+// 3. Declarar e inicializar
+let nombre = "Cata";
+let edad;
+edad = 30; 
+
+// 4. Concatenar ciudad y país
+let ciudad = "Medellín";
+let pais = "Colombia";
+let residencia = ciudad + ", " + pais;
+console.log(residencia);
+
+// 5. Mensaje final
+console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${residencia}`);
